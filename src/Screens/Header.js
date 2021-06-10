8452e1b707a22d718c7d40268318ffc6db83b2bb
jaspreet39 <jassingh0244@gmail.com>
2021-06-10 13:13:54 +0530

    commit

--- src/Screens/Header.js
+++ src/Screens/Header.js
@@ -3,15 +3,23 @@ import './Header.css'
 import HeaderOption from './HeaderOption'
 import SearchIcon from '@material-ui/icons/Search';
 import AddIcon from '@material-ui/icons/Add';
-import { useHistory } from 'react-router';
-import { useSelector } from 'react-redux';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { useHistory, useLocation} from 'react-router';
+import { useSelector} from 'react-redux';
 import { selectUser } from '../features/userSlice';
 
 
 function Header() {
     const [show, handleShow] = useState(false);
+    const [burgerStatus, setBurgerStatus] = useState(false)
     const history = useHistory()
     const user = useSelector(selectUser)
+
+    const location = useLocation()
+    const pathName = location.pathname
+    console.log(pathName)
+
     useEffect(() => {
         window.addEventListener("scroll", () => {
             if (window.scrollY > 100) {
@@ -36,13 +44,26 @@ function Header() {
            <img onClick={()=>history.push("/")} src="https://firebasestorage.googleapis.com/v0/b/weshare-clone.appspot.com/o/assets%2Flogo.png?alt=media&token=e2497325-697f-4c6a-9838-fb19c67c46bd" alt=""/>
            <div className="header_right">
                <div className="header_rightIcons">
-                <HeaderOption Icon={SearchIcon} title="Find a rider"/>
+                <HeaderOption  onClick={()=>history.push("/")} Icon={SearchIcon} title="Find a rider" selected={pathName==="/"&& true} color="black"/>
+                <HeaderOption onClick={()=>history.push('/driverOffer')}  Icon={AddIcon} title="Offer a ride" selected={pathName==="/driverOffer"&& true} color="black"/>
+                <HeaderOption  avatar={true} title={user.displayName} selected={pathName==="/profile"&& true} color="black"/>
+               </div> 
+               <div className="header_menu">
+                    <MenuIcon onClick={()=>setBurgerStatus(true)}/>
+               </div>  
+           </div>
+           <div className="header_menuItems" style={{transform:burgerStatus ?"translateX(0)" : "translateX(100%)"}}>
+               <div className="header_closeIcon">
+                    <p>Menu Items</p>
+                    <CloseIcon onClick={()=>setBurgerStatus(false)}/>
+               </div>
+               <HeaderOption onClick={()=>history.push("/")} Icon={SearchIcon} title="Find a rider"/>
                 <HeaderOption onClick={()=>history.push('/driverOffer')}  Icon={AddIcon} title="Offer a ride"/>
                 <HeaderOption  avatar={true} title={user.displayName}/>
-               </div>   
            </div>
         </div>
     )
 }
+
 
 export default Header
